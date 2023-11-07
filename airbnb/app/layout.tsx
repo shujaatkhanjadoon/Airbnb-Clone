1da76970eb632f5c1895a656b03ec20742b97989
Shujaat Khan <148489139+shujaatkhanjadoon@users.noreply.github.com>
2023-11-07 17:20:36 +0000

    issues fixed

--- airbnb/app/layout.tsx
+++ airbnb/app/layout.tsx
@@ -1,16 +1,22 @@
-import { Nunito } from 'next/font/google';
-import type { Metadata } from 'next';
-import './globals.css';
-import Navbar from './components/navbar/Navbar';
-import ClientOnly from './components/ClientOnly'
-import Modal from './components/modals/Modal';
+import { Nunito } from 'next/font/google'
+
+import Navbar from '@/app/components/navbar/Navbar';
+import LoginModal from '@/app/components/modals/LoginModal';
+import RegisterModal from '@/app/components/modals/RegisterModal';
+import SearchModal from '@/app/components/modals/SearchModal';
+import RentModal from '@/app/components/modals/RentModal';
+
+import ToasterProvider from './providers/ToasterProvider';
+
+import './globals.css'
+import ClientOnly from './components/ClientOnly';
 
 
 const font = Nunito({
   subsets: ['latin'],
 })
 
-export const metadata: Metadata = {
+export const Metadata = {
   title: 'AirBnb | Shujaat Khan',
   description: 'Clone By Shujaat Khan',
 }
@@ -20,14 +26,25 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
+  // const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-        <Modal title="Login In" isOpen />
-        <Navbar/>
+          <ToasterProvider />
+          <LoginModal />
+          <RegisterModal />
+          <SearchModal />
+          <RentModal />
+          {/* <Navbar currentUser={currentUser} /> */}
+          <Navbar />
         </ClientOnly>
-        {children}</body>
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
