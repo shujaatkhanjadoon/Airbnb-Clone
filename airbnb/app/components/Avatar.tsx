b52b93af761ed5364b0f65f1ad4b4e233d5f4815
Shujaat Khan <148489139+shujaatkhanjadoon@users.noreply.github.com>
2023-11-08 09:13:40 +0000

    img

--- airbnb/app/components/Avatar.tsx
+++ airbnb/app/components/Avatar.tsx
@@ -1,15 +1,21 @@
 'use client';
-import Image from 'next/image';
 
-const Avatar = ()=> {
-    return (
-        <Image 
-        className="rounded-full"
-        height="30"
-        width="30"
-        alt="Avatar"
-        src="/images/placeholder.jpeg"
-         />
-    );
+import Image from "next/image";
+
+interface AvatarProps {
+  src: string | null | undefined;
 }
+
+const Avatar: React.FC<AvatarProps> = ({ src }) => {
+  return ( 
+    <Image 
+      className="rounded-full" 
+      height="30" 
+      width="30" 
+      alt="Avatar" 
+      src={src || '/images/placeholder.jpg'}
+    />
+   );
+}
+ 
 export default Avatar;
